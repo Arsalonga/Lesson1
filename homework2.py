12e7cf78bfc2f1d41775b2ae3b8494c1e1635720
Arsalonga <arsa.longa@mail.ru>
2024-07-01 06:51:04 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+quantity_hmwrk=12
+hour=1.5
+name_course='Python'
+time_for_one_lesson=hour/quantity_hmwrk
+print('Курс: ',name_course,', всего задач:',quantity_hmwrk,', затрачено часов: ',hour,', среднее время выполнения ',time_for_one_lesson,' часа.')
